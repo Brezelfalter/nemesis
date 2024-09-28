36c06b67c99c247b38b4c3fc2b5a4857e8d29773
Brezelfalter <lukas.neumanntuf@gmail.com>
2024-09-28 15:37:28 +0200

    added network, gateway setup file

---
+++ network/gateway.sh
@@ -0,0 +1,9 @@
+interface=enp2s0
+
+sudo iptables -A FORWARD -i $interface -o $interface -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+sudo iptables -A FORWARD -i $interface -o $interface -j ACCEPT
+
+sudo iptables  --append FORWARD --in-interface $interface -j ACCEPT
+
+sudo iptables --table nat --append POSTROUTING --out-interface $interface -j MASQUERADE
